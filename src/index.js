fee5b5fe908c70b66eb73597920f0657a06cd92e
AmberAAA <qe-d@163.com>
2019-01-16 13:04:57 +0800

    add editorconfig & eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import './index.scss'
-import './index.css'
+import "./index.scss";
+import "./index.css";
 
+/* eslint-disable-next-line */
 console.log("hello");
-[1,2,3,4].map(a => {
+[1, 2, 3, 4].map(a => {
+    /* eslint-disable-next-line */
     console.log(a + 1);
-})
+});
